1e9ef5f04a92fe41dbfae31df80ff325a182966d
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-07-30 15:11:29 +0530

    Add New & Rename Project Components

--- todo-app/src/components/Project.js
+++ todo-app/src/components/Project.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React,{useState} from "react";
 import RenameProject from "./RenameProject";
+import Modal from './Modal'
 import { Pencil, XCircle } from "react-bootstrap-icons";
 
 function Project({ project, edit }) {
+  const [showModal,setShowModal]=useState(false)
   return (
     <div className="Project">
       <div className="name">{project.name}</div>
       <div className="btns">
         {edit ? (
           <div className="edit-delete">
-            <span className="edit">
+            <span className="edit" onClick={()=> setShowModal(true)}>
               <Pencil size="13" />
             </span>
             <span className="delete">
@@ -22,6 +24,9 @@ function Project({ project, edit }) {
           <div className="total-todos">{project.numOfTodos}</div>
         )}
       </div>
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <RenameProject project={project} setShowModal={setShowModal}/>
+      </Modal>
     </div>
   );
 }
--- todo-app/src/components/RenameProject.js
+++ todo-app/src/components/RenameProject.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
+import ProjectForm from './ProjectForm'
 
-function RenameProject(){
+function RenameProject({project,setShowModal}){
+    const [newProjectName,setNewProjectName] = useState(project.name)
+    function handleSubmit(e){
+
+    }
     return(
         <div className='RenameProject'>
-           RenameProject
+            <ProjectForm
+                    handleSubmit={handleSubmit}
+                    heading='Edit Project Name'
+                    value={newProjectName}
+                    setValue={setNewProjectName}
+                    setShowModal={setShowModal}
+                    confirmButtonText='confirm'
+                />
         </div>
     )
 }
