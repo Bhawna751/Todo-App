9d7ad2b7b0bac5f50370ab65842f8b3de482bc12
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-07-30 09:14:27 +0530

    Creating a modal

--- todo-app/src/components/AddNewTodo.js
+++ todo-app/src/components/AddNewTodo.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
+import Modal from "./Modal";
 
-function AddNewTodo(){
-    return(
-        <div className='AddNewTodo'>
-           AddNewTodo
+function AddNewTodo() {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <div className="AddNewTodo">
+      <div className="btn">
+        <button onClick={()=>setShowModal(true)}>+ New Todo</button>
+      </div>
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <div>Hello
+            <button onClick={()=> setShowModal(false)}>
+                hide
+            </button>
         </div>
-    )
+      </Modal>
+    </div>
+  )
 }
 
-export default AddNewTodo
+export default AddNewTodo;
--- todo-app/src/components/User.js
+++ todo-app/src/components/User.js
@@ -6,10 +6,10 @@ function User() {
     <div className="User">
       <div className="logo">
         <img src={logo} alt="logo" />
-        <div className="info">
-          <p>Todo App</p>
-          <a href="#">Logout!</a>
-        </div>
+      </div>
+      <div className="info">
+        <p>Todo App</p>
+        <a href="#">Logout!</a>
       </div>
     </div>
   );
